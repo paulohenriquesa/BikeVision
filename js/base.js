97f413dbff17b2b12b613a5007b86eea7b81032e
paulo1912 <paulo1912@gmail.com>
2016-06-05 12:11:45 -0300

    Update base.js

--- js/base.js
+++ js/base.js
@@ -38,12 +38,12 @@ function initialize(){
 	var osmURL = 'https:\/\/a.tiles.mapbox.com\/v4\/mapbox.streets\/{z}\/{x}\/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbTgzcHQxMzAxMHp0eWx4bWQ1ZHN2NGcifQ.WVwjmljKYqKciEZIC3NfLA';
 	var cyclesURL =  'http://tile.lonvia.de/cycling/{z}/{x}/{y}.png';
 
-	var streets = L.tileLayer(osmURL,{maxZoom: 15});
-	var cycle = L.tileLayer(cyclesURL,{maxZoom: 15});
+	var streets = L.tileLayer(osmURL,{maxZoom: 12});
+	var cycle = L.tileLayer(cyclesURL,{maxZoom: 12});
 	
 	var map = L.map('map',{
 		center: [-3.7304512, -38.5217989],
-		zoom: 15,
+		zoom: 12,
 		layers: [streets, cycle, bicicletar, bicicletario, airPump, bicicletaria]
 	});
 	var base = {
